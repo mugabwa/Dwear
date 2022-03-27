d8ebd5b274f9f2553c6f561a92915c7b0e410987
mugabwa <mugabwaallanmas@gmail.com>
2022-03-27 10:12:52 +0300

    update view

--- routes/views.py
+++ routes/views.py
@@ -1,8 +1,17 @@
 from django.views.generic import ListView
+from django.views.generic.edit import UpdateView
 from django.shortcuts import render
 from .models import Route
-# Create your views here.
+
+
 class RouteListView(ListView):
     model = Route
     template_name = 'route_list.html'
     context_object_name = 'routes'
+
+
+class RouteUpdate(UpdateView):
+    model = Route
+    fields = ['origin','destination','distance','condition','cost']
+    template_name = 'route_update_form.html'
+    context_object_name = 'route'
