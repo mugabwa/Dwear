e6e345a45c9196120180be2995c9fc61842fe830
Mugabwa <mugabwaallanmas@gmail.com>
2022-03-27 08:10:20 +0000

    Changes on update view

--- routes/urls.py
+++ routes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RouteListView
+from .views import RouteListView, RouteUpdate
 
 urlpatterns = [
     path('routes/',RouteListView.as_view(), name='route-list'),
+    path('routes/<int:pk>/update', RouteUpdate.as_view(), name='route-update'),
 ]
