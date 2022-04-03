37aaa33c0577b73a3cbbf2f5bf65060e4bbe7e87
Mugabwa <mugabwaallanmas@gmail.com>
2022-04-03 09:31:34 +0000

    authentication

--- Dwear/settings.py
+++ Dwear/settings.py
@@ -124,6 +124,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL = 'route-list'
+LOGIN_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
--- client/views.py
+++ client/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 
 from .forms import UserForm
 
@@ -7,7 +8,11 @@ def create_user(request):
         form = UserForm(request.POST)
         if form.is_valid():
             form.save()
-            
+            email = form.changed_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = authenticate(email=email, password=password)
+            login(request, user)
+            return redirect('route-list')
     else:
         form = UserForm()
     return render(request, 'create_user.html', {'form': form})
